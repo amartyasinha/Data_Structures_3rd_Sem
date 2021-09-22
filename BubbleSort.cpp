0e811dbd9edbc0f48f08b3c2572543894cb4c53c
amartya <amartyasinha918@gmail.com>
2021-09-22 19:50:27 +0530

    Commit DS Files

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -29,8 +29,8 @@ int main() {
 
     int temp;
 
-    for (int i = 0; i<n; i++){
-        for (int j = 0; j<n-i; j++){
+    for (int i = 0; i<n-1; i++){
+        for (int j = 0; j<n-i-1; j++){
             if (array[j]>array[j+1]){
                 temp = array[j];
                 array[j] = array[j+1];
